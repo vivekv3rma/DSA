2323a8d59e83a8dab1d50a718bdf1aa2a01559fd
Vivek Verma <vivekv3rma@gmail.com>
2022-02-20 17:48:46 +0530

    Updated readme and bubble sort test case

--- DSA/Program.cs
+++ DSA/Program.cs
@@ -17,6 +17,12 @@ namespace DSA
             Utility.printArray(arr);
             sort.sort(arr);
             Utility.printArray(arr);
+            
+            //testing sorted array
+            int[] arr1= { 1,2,3,4,5,6};
+            Utility.printArray(arr1);
+            sort.sort(arr1);
+            Utility.printArray(arr1);
 
         }
     }
---
+++ DSA/README.md
@@ -0,0 +1,4 @@
+DSA
+Started with DSA basic and practicing git along with it.
+
+1. Added first sorting algorithm - BubbleSort
